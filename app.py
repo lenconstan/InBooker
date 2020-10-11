0c967df5b6e72ad7f8717064caf1855814a3c8d5
lenconstan <kruize.cl@gmail.com>
2020-10-11 12:03:49 +0200

    added redis

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+import os
+import redis
 from flask import Flask
+
 app = Flask(__name__)
+db=redis.from_url(os.environ['REDISCLOUD_URL'])
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    name=db.get('name') or'World'
+    return 'Hello %s!' % name
+
+@app.route('/setname/<name>')
+def setname(name):
+    db.set('name',name)
+    return 'Name updated.'
 
 if __name__ == '__main__':
     app.run()
